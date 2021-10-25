d53715b6353281313ad0c7f0a0d0f17bea25711b
AnneLund <89774650+AnneLund@users.noreply.github.com>
2021-10-25 07:32:57 +0200

    Add files via upload

---
+++ Tolne Naturretreat hjemmeside/Script/script.js
@@ -0,0 +1,56 @@
+// sidebar open close pi code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function () {
+  navLinks.style.left = "0";
+};
+menuCloseBtn.onclick = function () {
+  navLinks.style.left = "-100%";
+};
+
+// sidebar submenu open close pi code
+let aktiviteterArrow = document.querySelector(".aktiviteter-arrow");
+aktiviteterArrow.onclick = function () {
+  navLinks.classList.toggle("show1");
+};
+
+let piArrow = document.querySelector(".pi-arrow");
+piArrow.onclick = function () {
+  navLinks.classList.toggle("show3");
+};
+
+// SLIDESHOW //
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+// DROPDOWN-LIST //
+
+function myFunction() {
+  var x = document.getElementById("Demo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
